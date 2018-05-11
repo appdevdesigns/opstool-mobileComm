c038c2047ae086381c0c53ea4043738c64ca9b01
Johnny Hausman <johnny@hausmanfamily.com>
2018-05-11 11:39:58 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+opstool-mobilecomm ![Build Status](https://travis-ci.org/appdevdesigns/opstool-mobilecomm.png)
+================
+
+A new plugin.
---
+++ config/routes.js
@@ -0,0 +1,29 @@
+/**
+ * Routes
+ *
+ * Use this file to add any module specific routes to the main Sails
+ * route object.
+ */
+
+
+module.exports = {
+
+
+  /*
+
+  '/': {
+    view: 'user/signup'
+  },
+  '/': 'opstool-mobilecomm/PluginController.inbox',
+  '/': {
+    controller: 'opstool-mobilecomm/PluginController',
+    action: 'inbox'
+  },
+  'post /signup': 'opstool-mobilecomm/PluginController.signup',
+  'get /*(^.*)' : 'opstool-mobilecomm/PluginController.profile'
+
+  */
+
+
+};
+
